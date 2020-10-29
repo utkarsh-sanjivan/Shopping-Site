1344580701260384f2f0d8e37087b1eb842f2399
Utkarsh <utkarsh.sannu@gmail.com>
2020-10-29 17:30:31 +0530

    Added Logo, Title and Browser Icon

--- src/component/organisms/Header/index.js
+++ src/component/organisms/Header/index.js
@@ -4,7 +4,8 @@ import './index.css';
 export default function Header(props) {
     return (
         <div className='header'>
-            <div className='logo-header'>Logo</div>
+            <div className='logo-header'></div>
+            {/* <img src='/cart-logo.png' alt='shopping-cart' /> */}
             <div className='title-header'>Shopping Site</div>
         </div>
     )
