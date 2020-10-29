de19a13b5bfb23a70679efa6de91d00f2fc7a9a7
Utkarsh <utkarsh.sannu@gmail.com>
2020-10-29 17:51:32 +0530

    Minor Fix

--- src/container/CatalogPage/index.js
+++ src/container/CatalogPage/index.js
@@ -39,7 +39,7 @@ export default function CatalogPage(props) {
             setCartItem([
                 ...cartItem,
                 tempCart,
-            ])
+            ]);
         }
     }
 
